882aa02d0b2ba21cf26615b128f341b584fde6cc
JGWill <jgi@jgwill.com>
2024-09-20 18:55:57 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jghfmanager',
-    version='0.1.4',
+    version='0.1.5',
     author='J.Guillaume D-Isabelle',
     description='A Python module jghfmanager',
     url='https://github.com/jgwill/jghfmanager',
